3ed2ba0307e52916b553e7aba1f70cb97c4bb9eb
Davide <davide.roberti89@gmail.com>
2020-02-19 15:23:26 +0100

    fine mail

--- mail/js/master.js
+++ mail/js/master.js
@@ -1,1 +1,24 @@
+//creare un array con una lista di mail
+// chiedere all'utente la sua Mail
+//verificare se la mail corrisponde a uno dei campi dell'array
+//se vorrisponde comunico all'utente che può accedere
+//se non corrisponde comunico all'utente che non può accedere
 
+var indirizzoMail = ['paolino@mail.com', 'pancrazio@mail.it', 'sigismondo@mail.com', 'lucrezio@mail.it'];
+console.log(indirizzoMail);
+
+var mailUtente = prompt('inserisci il tuo indirizzo mail');
+
+var accesso = 'non consentito';
+
+for (var i = 0; i < indirizzoMail.length; i++) {
+    if (mailUtente == indirizzoMail[i]) {
+        var accesso = 'consentito';
+    }
+}
+
+if (accesso == 'consentito') {
+    document.getElementById('conferma-accesso').innerHTML = 'puoi accedere';
+} else {
+    document.getElementById('conferma-accesso').innerHTML = 'non puoi accedere';
+}
